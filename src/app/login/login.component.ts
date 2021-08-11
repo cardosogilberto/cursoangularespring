074a8b87bfc0be845ee1a91c7d066911fdb25645
Gilberto Cardoso <gilberto.cardoso537@gmail.com>
2021-08-11 20:28:58 -0300

    Melhorias formulario login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,6 +42,10 @@ export class LoginComponent {
       .salvar(usuario)
       .subscribe(response => {
         this.mensagemSucesso = "Cadastro Efetuado com sucesso ! Efetue o login."
+        this.cadastrando = false;
+        this.username = '';
+        this.password = '';
+        this.errors=[];
       }, errorResponse => {
         this.mensagemSucesso = null;
         this.errors = errorResponse.error.errors;
