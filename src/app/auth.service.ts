0a00ced8114854f7c1cda64498484180c54b629e
Administrador <gilberto.cardoso537@gmail.com>
2021-08-14 18:42:24 -0300

    Controlando sessao usuario com token

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { Usuario } from './login/usuario';
+import { JwtHelperService } from '@auth0/angular-jwt'
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +15,28 @@ export class AuthService {
   clientID: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
 
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(
     private http: HttpClient
   ) { }
 
-  isAuthenticated(): boolean {
-    return true;
+  obterToken() {
+    const tokenString = localStorage.getItem('access_token')
+    if (tokenString) {
+      const token = JSON.parse(tokenString).access_token;
+      return token;
+    }
+    return null;
+  }
 
+  isAuthenticated(): boolean {
+    const token = this.obterToken();
+    if (token) {
+      const expired = this.jwtHelper.isTokenExpired(token);
+      return !expired;
+    }
+    return true;
   }
 
   salvar(usuario: Usuario): Observable<any> {
