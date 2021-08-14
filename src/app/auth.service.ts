a80674aeaf4a8053e0c662d0b93900a9c6c171b6
Gilberto Cardoso <gilberto.cardoso537@gmail.com>
2021-08-14 16:07:47 -0300

    Utilizando guards protecao rotas

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -19,6 +19,10 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  isAuthenticated(): boolean {
+    return true;
+  }
+
   salvar(usuario: Usuario): Observable<any> {
     return this.http.post<any>(this.apiURL, usuario);
   }
