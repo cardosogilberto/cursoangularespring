7c89f3cfffb7e837b8c5ce9288ef26e1f0794ffc
cardosogilberto <gilberto.cardoso537@gmail.com>
2021-09-29 20:57:25 -0300

    modal confirmacao de delecao cliente

--- clientes-app/src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ clientes-app/src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
 import { Router } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-clientes-lista',
@@ -12,6 +13,7 @@ import { Router } from '@angular/router';
 export class ClientesListaComponent implements OnInit {
 
   clientes: Cliente[] = [];
+  clienteSelecionado: Cliente;
 
   constructor(
     private service: ClientesService,
@@ -25,6 +27,10 @@ export class ClientesListaComponent implements OnInit {
 
   novoCadastro() {
     this.router.navigate(['/clientes-form'])
+  }
+
+  preparaDelecao(cliente: Cliente) {
+    this.clienteSelecionado = cliente;
   }
 
 }
